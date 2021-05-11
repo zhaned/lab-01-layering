7f250de37e7aa6aabe4bf36ac4dfe18cc1a05a92
zhaned <zhanedmond@gmail.com>
2021-05-11 09:02:05 -0700

    refactoring and stuff

--- src/components/colors/Display.jsx
+++ src/components/colors/Display.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
-
 const Display = ({ bgcolor, bgImage }) => (
-
-<div style={{
-    width: '634px',
-    height: '634px',
-    backgroundColor: bgcolor,
-    backgroundImage: `url("${bgImage}")`
-  }}></div>
-
+  <div
+    style={{
+      width: '634px',
+      height: '634px',
+      backgroundColor: bgcolor,
+      backgroundImage: `url("${bgImage}")`,
+    }}
+  ></div>
 );
 
-
+Display.propTypes = {
+  bgcolor: PropTypes.string.isRequired,
+  bgImage: PropTypes.string.isRequired,
+};
 
 export default Display;
--- src/containers/RandomColor.jsx
+++ src/containers/RandomColor.jsx
@@ -4,37 +4,34 @@ import Display from '../components/colors/Display';
 export default class randomColor extends Component {
   state = {
     color: 'grey',
-    image: ''
-  }
+    image: '',
+  };
 
   changeColor = () => {
-    const colorArray = [
-      'red', 'blue', 'green', 'cyan', 'yellow', 'orange'
-    ];
+    const colorArray = ['red', 'blue', 'green', 'cyan', 'yellow', 'orange'];
     let randColor = colorArray[Math.floor(Math.random() * colorArray.length)];
     return randColor;
   };
 
-  componentDidMount(){
+  componentDidMount() {
     this.interval = setInterval(() => {
       let newColor = this.changeColor();
-      if(newColor === this.state.color){
+      if (newColor === this.state.color) {
         this.setState({
           color: '',
-          image: 'https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg'
-        })
-      } else{
-      this.setState({
-        color: newColor,
-        image: ''
-      })
+          image:
+            'https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg',
+        });
+      } else {
+        this.setState({
+          color: newColor,
+          image: '',
+        });
       }
-    }, 1800)
+    }, 1800);
   }
 
   render() {
-    return (
-      <Display bgcolor={this.state.color} bgImage={this.state.image}/>
-    )
+    return <Display bgcolor={this.state.color} bgImage={this.state.image} />;
   }
 }
